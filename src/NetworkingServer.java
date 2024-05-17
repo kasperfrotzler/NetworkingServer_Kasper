1f8e22c1872506e08068bf63170deafeb4352408
KasperFrotzlerElev <128380069+KasperFrotzlerElev@users.noreply.github.com>
2024-05-17 19:11:58 +0200

    klar

---
+++ src/NetworkingServer.java
@@ -0,0 +1,51 @@
+import java.net.*;
+import java.io.*;
+public class NetworkingServer {
+
+    public static void main(String[] args) {
+        ServerSocket server = null;
+        Socket client;
+        int portnumber = 1337;
+        if (args.length >= 1){
+            portnumber = Integer.parseInt(args[0]);
+        }
+        try{
+            server = new ServerSocket(portnumber);
+        } catch (IOException ie){
+            System.out.println("Cannot open socket." +ie);
+            System.exit(1);
+        }
+        System.out.println("ServerSocket is created " +server);
+
+        while(true){
+            try{
+                System.out.println("Waiting for connect request...");
+                client = server.accept();
+
+                System.out.println("Connect request is accepted...");
+                String cleintHost = client.getInetAddress().getHostAddress();
+                int clientPort = client.getPort();
+                System.out.println("Client host = " + cleintHost+"Client port = "+clientPort);
+
+                InputStream clientIn = client.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(clientIn));
+                String msgFromClient = br.readLine();
+                System.out.println("Message received from client = "+msgFromClient);
+
+                if(msgFromClient != null && !msgFromClient.equalsIgnoreCase("bye")){
+                    OutputStream clientOut = client.getOutputStream();
+                    PrintWriter pw = new PrintWriter(clientOut, true);
+                    String ansMsg = "Hello, " +msgFromClient;
+                    pw.println(ansMsg);
+                }
+                if (msgFromClient != null && msgFromClient.equalsIgnoreCase("bye")){
+                    server.close();
+                    client.close();
+                    break;
+                }
+            } catch (IOException ie) {
+                System.out.println("Connection failed");
+            }
+        }
+    }
+}
